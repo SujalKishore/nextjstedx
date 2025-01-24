fb84dec94a13e210ca0b4c880b29c3fb7da2371f
Zain Khan <142525731+Zain-Khan05@users.noreply.github.com>
2025-01-24 22:24:40 +0530

    GIVE ME SPACE

--- src/components/ui/morphing-text.tsx
+++ src/components/ui/morphing-text.tsx
@@ -1,60 +1,45 @@
 "use client";
-
 import { useCallback, useEffect, useRef } from "react";
-
 import { cn } from "@/lib/utils";
-
 const morphTime = 1.5;
 const cooldownTime = 0.5;
-
 const useMorphingText = (texts: string[]) => {
   const textIndexRef = useRef(0);
   const morphRef = useRef(0);
   const cooldownRef = useRef(0);
   const timeRef = useRef(new Date());
-
   const text1Ref = useRef<HTMLSpanElement>(null);
   const text2Ref = useRef<HTMLSpanElement>(null);
-
   const setStyles = useCallback(
     (fraction: number) => {
       const [current1, current2] = [text1Ref.current, text2Ref.current];
       if (!current1 || !current2) return;
-
       current2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
       current2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
       const invertedFraction = 1 - fraction;
       current1.style.filter = `blur(${Math.min(
         8 / invertedFraction - 8,
         100
       )}px)`;
       current1.style.opacity = `${Math.pow(invertedFraction, 0.4) * 100}%`;
-
       current1.textContent = texts[textIndexRef.current % texts.length];
       current2.textContent = texts[(textIndexRef.current + 1) % texts.length];
     },
     [texts]
   );
-
   const doMorph = useCallback(() => {
     morphRef.current -= cooldownRef.current;
     cooldownRef.current = 0;
-
     let fraction = morphRef.current / morphTime;
-
     if (fraction > 1) {
       cooldownRef.current = cooldownTime;
       fraction = 1;
     }
-
     setStyles(fraction);
-
     if (fraction === 1) {
       textIndexRef.current++;
     }
   }, [setStyles]);
-
   const doCooldown = useCallback(() => {
     morphRef.current = 0;
     const [current1, current2] = [text1Ref.current, text2Ref.current];
