b4bce455c6d6a072b469cf9cfd9ada90efcf1e03
Zain Khan <142525731+Zain-Khan05@users.noreply.github.com>
2025-01-24 19:44:29 +0530

    spaceeeeeeeeeee

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import {Mona_Sans} from 'next/font/google';
 import Ticket from "@/components/Ticket/Ticket";
-
 const mona = Mona_Sans({
   subsets:['latin'],
 });
-
 const Register: React.FC = () => {
   return (
     <div className={mona.className}>
@@ -13,5 +11,4 @@ const Register: React.FC = () => {
     </div>
   );
 };
-
 export default Register;
