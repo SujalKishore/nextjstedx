a981b955d03c6d6d4f55d0ea27aeef3ff9d5fc96
Zain Khan <142525731+Zain-Khan05@users.noreply.github.com>
2025-01-24 22:13:40 +0530

    vishodan

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -16,7 +15,6 @@ const Card = React.forwardRef<
   />
 ));
 Card.displayName = "Card";
-
 const CardHeader = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -28,7 +26,6 @@ const CardHeader = React.forwardRef<
   />
 ));
 CardHeader.displayName = "CardHeader";
-
 const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
@@ -43,7 +40,6 @@ const CardTitle = React.forwardRef<
   />
 ));
 CardTitle.displayName = "CardTitle";
-
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
@@ -55,7 +51,6 @@ const CardDescription = React.forwardRef<
   />
 ));
 CardDescription.displayName = "CardDescription";
-
 const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -63,7 +58,6 @@ const CardContent = React.forwardRef<
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
 ));
 CardContent.displayName = "CardContent";
-
 const CardFooter = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -75,7 +69,6 @@ const CardFooter = React.forwardRef<
   />
 ));
 CardFooter.displayName = "CardFooter";
-
 export {
   Card,
   CardHeader,
