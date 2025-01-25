e6601c10c36def1238d6da5e3dcbca2ec934d667
SujalKishore <131854159+SujalKishore@users.noreply.github.com>
2025-01-26 03:41:30 +0530

    eh

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -4,7 +4,7 @@ import DigitalWalls from "@/components/DigitalWalls/DigitalWalls";
 const PageWithCountdownAndDigitalWalls = () => {
   return (
     <div>
-      <section className="h-[65vh] relative mt-44">
+      <section className="h-[80vh] relative mt-36">
         <div className="absolute w-48 h-48 bg-white/5 transform rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
         <div className="absolute w-72 h-72 border-white/10 border-2 transform rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
         <div className="absolute w-24 h-24 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
