b579f3fcac740e85972d6ffa69b96077e429404f
Arjun-143-kamath <arjun.kamath24@st.niituniversity.in>
2025-04-21 14:15:56 +0530

    Timer fixxxx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Footer from "@/components/Footer/Footer";
 import CustomTimer from "@/components/Timer/Timer";
 import { BulletinBoard } from "@/components/BulletinBoard/BulletinBoard";
 import Link from "next/link";
+import confetti from "canvas-confetti";
 
 interface InnovativeSectionProps {
   title: string;
@@ -80,6 +81,42 @@ const HeroSection = () => {
   const [isMobile, setIsMobile] = useState(false);
   const heroRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
+
+  
+  const handleConfetti = () => {
+    const duration = 3 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+ 
+    const randomInRange = (min: number, max: number) =>
+      Math.random() * (max - min) + min;
+ 
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+ 
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+ 
+      const particleCount = 50 * (timeLeft / duration);
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    },250);
+  };
+
+  useEffect(() => {
+    if (Date() >= "Mar 09 2025 08:30:00") {
+      handleConfetti()
+    }
+  }, [])
 
   useEffect(() => {
     const handleResize = () => {
